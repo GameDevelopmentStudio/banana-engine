1e89be912e5c6c33816b8e4163fc886e8f7874ec
Rafa de la Hoz <rafadelahoz@gmail.com>
2012-12-31 14:54:49 +0100

    Added engine basic classes and a base game class.

---
+++ Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BananaEngine
+{
+    public class BananaConfig
+    {
+        public static bool DEBUG = true;
+    }
+}
